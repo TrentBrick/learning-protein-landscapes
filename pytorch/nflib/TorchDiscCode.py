more debugging of the discrete flows and fixed NICE masking bug

pytorch/nflib/TorchDiscCode.py
@@ -236,7 +236,7 @@ class DiscreteAutoregressiveFlow(nn.Module):
     #  self._maybe_build(inputs)
 
     net = self.layer(inputs, **kwargs)
-    print('shape of neural net output', net.shape)
+    #print('shape of neural net output', net.shape)
     #print('reverse net output', net.shape)
     if net.shape[-1] == 2 * self.vocab_size:
       loc, scale = torch.split(net, self.vocab_size, dim=-1)
@@ -249,7 +249,7 @@ class DiscreteAutoregressiveFlow(nn.Module):
       raise ValueError('Output of layer does not have compatible dimensions.')
     loc = disc_utils.one_hot_argmax(loc, self.temperature).type(inputs.dtype)
     #print('reverse loc', loc.shape)
-    outputs = disc_utils.one_hot_add(scaled_inputs, loc )
+    outputs = disc_utils.one_hot_add(scaled_inputs, loc)
     #print(outputs.shape)
     return outputs
 
@@ -291,7 +291,7 @@ class DiscreteBipartiteFlow(nn.Module):
   [prime fields](https://en.wikipedia.org/wiki/Finite_field)).
   """
 
-  def __init__(self, layer, mask, temperature, vocab_size):
+  def __init__(self, layer, parity, temperature, vocab_size, dim):
     """Constructs flow.
     Args:
       layer: Two-headed masked network taking the inputs and returning a
@@ -305,9 +305,10 @@ class DiscreteBipartiteFlow(nn.Module):
     """
     super().__init__()
     self.layer = layer
-    self.mask = torch.tensor(mask).float()
+    self.parity = parity # going to do a block split. #torch.tensor(mask).float()
     self.temperature = temperature
     self.vocab_size = vocab_size
+    self.dim = dim # total dimension of the vector being dealt with. 
 
   '''def __call__(self, inputs, *args, **kwargs):
     if not isinstance(inputs, random_variable.RandomVariable):
@@ -317,48 +318,75 @@ class DiscreteBipartiteFlow(nn.Module):
 
   def forward(self, inputs, **kwargs):
     """Forward pass for bipartite generation."""
+    #TODO: implement even odd shuffling. 
     batch_ndims = len(inputs.shape) - 2
-    #mask = self.mask.view([1] * batch_ndims + [-1, 1]) 
-    masked_inputs = self.mask * inputs
-    net = self.layer(masked_inputs, **kwargs)
-    if net.shape[-1] == 2 * self.vocab_size: # have a location and scaling parameter
-      loc, scale = torch.split(net, self.vocab_size, dim=-1)
-      loc = disc_utils.one_hot_argmax(loc, self.temperature).type( inputs.dtype)
+    #mask = self.mask.view([1] * batch_ndims + [-1, 1])
+     
+    assert len(inputs.shape) ==2, 'need to flatten the inputs first!!!'
+    z0, z1 = inputs[:,:self.dim//2], inputs[:,self.dim//2:]
+
+    if self.parity:
+      z0, z1 = z1, z0
+    #masked_inputs = self.mask * inputs
+    x0 = z0 
+    layer_outs = self.layer(x0, **kwargs)
+    if layer_outs.shape[-1] == 2 * self.vocab_size: # have a location and scaling parameter
+      loc, scale = torch.split(layer_outs, self.vocab_size, dim=-1)
+      loc = disc_utils.one_hot_argmax(loc, self.temperature).type(inputs.dtype)
       scale = disc_utils.one_hot_argmax(scale, self.temperature).type(inputs.dtype)
       inverse_scale = disc_utils.multiplicative_inverse(scale, self.vocab_size)
-      shifted_inputs = disc_utils.one_hot_minus(inputs, loc)
-      masked_outputs = (1.0 - mask) * disc_utils.one_hot_multiply(shifted_inputs,
+      shifted_inputs = disc_utils.one_hot_minus(z1, loc)
+
+      x1 = disc_utils.one_hot_multiply(shifted_inputs,
                                                             inverse_scale)
-    elif net.shape[-1] == self.vocab_size:
-      loc = net
+      #masked_outputs = (1.0 - self.mask) * disc_utils.one_hot_multiply(shifted_inputs,
+      #                                                      inverse_scale)
+
+    elif layer_outs.shape[-1] == self.vocab_size:
+      loc = layer_outs
       loc = disc_utils.one_hot_argmax(loc, self.temperature).type( inputs.dtype)
-      masked_outputs = (1.0 - mask) * disc_utils.one_hot_minus(inputs, loc)
+      x1 = disc_utils.one_hot_minus(z1, loc)
+      #masked_outputs = (1.0 - self.mask) * disc_utils.one_hot_minus(inputs, loc)
     else:
       raise ValueError('Output of layer does not have compatible dimensions.')
-    outputs = masked_inputs + masked_outputs
-    return outputs
+    x = torch.cat([x0, x1], dim=1)
+    return x
 
   def reverse(self, inputs, **kwargs):
     """Reverse pass for the inverse bipartite transformation."""
-    inputs = inputs
+    #print(inputs.shape)
+    assert len(inputs.shape) ==2, 'need to flatten the inputs first!!!'
     batch_ndims = len(inputs.shape) - 2
     #mask = self.mask.view([1] * batch_ndims + [-1, 1])
-    masked_inputs = self.mask * inputs
-    net = self.layer(masked_inputs, **kwargs)
-    print('net output shape', net.shape)
-    if net.shape[-1] == 2 * self.vocab_size:
-      loc, scale = torch.split(net, self.vocab_size, dim=-1)
+    #masked_inputs = self.mask * inputs
+
+    x0, x1 = inputs[:,:self.dim//2], inputs[:,self.dim//2:]
+    if self.parity:
+      x0, x1 = x1, x0
+    #masked_inputs = self.mask * inputs
+    z0 = x0 
+    layer_outs = self.layer(z0, **kwargs)
+    #print('net output shape', net.shape)
+    if layer_outs.shape[-1] == 2 * self.vocab_size:
+      loc, scale = torch.split(layer_outs, self.vocab_size, dim=-1)
       scale = disc_utils.one_hot_argmax(scale, self.temperature).type(inputs.dtype)
-      scaled_inputs = disc_utils.one_hot_multiply(inputs, scale)
-    elif net.shape[-1] == self.vocab_size:
-      loc = net
-      scaled_inputs = inputs
+      scaled_inputs = disc_utils.one_hot_multiply(x1, scale)
+    elif layer_outs.shape[-1] == self.vocab_size:
+      #print('no scaling')
+      loc = layer_outs
+      scaled_inputs = x1
     else:
       raise ValueError('Output of layer does not have compatible dimensions.')
+    #print('loc is', loc.shape)
+    #print(z0.shape)
     loc = disc_utils.one_hot_argmax(loc, self.temperature).type(inputs.dtype)
-    masked_outputs = (1.0 - mask) * disc_utils.one_hot_add(loc, scaled_inputs)
-    outputs = masked_inputs + masked_outputs
-    return outputs
+    # applying to everything that was not masked. 
+    z1 = disc_utils.one_hot_add(loc, scaled_inputs)
+    z = torch.cat([z0, z1], dim=1)
+
+    #print(z0[0].sum(), z1[0].sum(), 'full z', z[0].sum(), loc[0].sum(), scaled_inputs[0].sum(), 'x1 multi', disc_utils.one_hot_add(loc, scaled_inputs)[0].sum())
+    #print('-----------')
+    return z
 
   def log_det_jacobian(self, inputs):
     return torch.zeros((1)).type(inputs.dtype)

pytorch/nflib/disc_utils.py
@@ -7,11 +7,21 @@ import numpy as np
 
 def one_hot_argmax(inputs, temperature, axis=-1):
     """Returns one-hot of argmax with backward pass set to softmax-temperature."""
+    autoreg=False
+    if len(inputs.shape) == 3:
+        autoreg=True
+    
     vocab_size = inputs.shape[-1]
     hard = torch.argmax(inputs, dim=axis).flatten().long().unsqueeze(1) # for some reason needs to be of type long. 
-    z = torch.zeros((inputs.shape[0] * inputs.shape[1], vocab_size))
+    if autoreg:
+        z = torch.zeros((inputs.shape[0] * inputs.shape[1], vocab_size))
+    else: 
+        z = torch.zeros((inputs.shape[0], vocab_size))
     z.scatter_(1,hard,1)
-    z = z.view(inputs.shape[0], inputs.shape[1], vocab_size)
+    if autoreg:
+        z = z.view(inputs.shape[0], inputs.shape[1], vocab_size)
+    else: 
+        z = z.view(inputs.shape[0], vocab_size)
     soft = F.softmax(inputs / temperature, dim=axis)
     outputs = soft + (z - soft).detach()
     return outputs
@@ -35,8 +45,8 @@ def multiplicative_inverse(a, n):
     Returns:
         Tensor of same shape and dtype as a.
     """
-    a = torch.tensor(a)
-    n = torch.tensor(n)
+    #a = torch.tensor(a)
+    #n = torch.tensor(n)
     vocab_size = a.shape[-1]
     a_dtype = a.dtype
     sparse_a = torch.argmax(a, dim=-1)
@@ -112,7 +122,7 @@ def one_hot_minus(inputs, shift):
         Tensor of same shape and dtype as inputs.
     """
     # TODO(trandustin): Implement with circular conv1d.
-    inputs = torch.tensor(inputs)
+    #inputs = torch.tensor(inputs)
     shift = shift.type( inputs.dtype)
     vocab_size = inputs.shape[-1]
     # Form a [..., vocab_size, vocab_size] matrix. Each batch element of
@@ -137,17 +147,17 @@ def one_hot_add(inputs, shift):
     Returns:
         Tensor of same shape and dtype as inputs.
     """
-    # TODO(trandustin): Implement with circular conv1d.
-    inputs = torch.tensor(inputs)
-    shift = shift.type( inputs.dtype)
+    shift = shift.type(inputs.dtype)
     vocab_size = inputs.shape[-1]
     # Form a [..., vocab_size, vocab_size] matrix. Each batch element of
     # inputs will vector-matrix multiply the vocab_size x vocab_size matrix. This
     # "shifts" the inputs batch element by the corresponding shift batch element.
     shift_matrix = torch.stack([torch.roll(shift, i, dims=-1)
                             for i in range(vocab_size)], dim=-2)
+    #print('shift matrix pre transpose', shift_matrix)
     shift_matrix = torch.transpose(shift_matrix, -1, -2)
     outputs = torch.einsum('...v,...uv->...u', inputs, shift_matrix)
+    #print('where does the gradient gooooo', inputs, shift_matrix, outputs)
     return outputs
 
 def one_hot_multiply(inputs, scale):
@@ -164,7 +174,7 @@ def one_hot_multiply(inputs, scale):
     Tensor of same shape and dtype as inputs.
     """
     # TODO(trandustin): Implement with circular conv1d.
-    inputs = torch.tensor(inputs)
+    #inputs = torch.tensor(inputs)
     scale = scale.type( inputs.dtype)
     batch_shape = list(inputs.shape[:-1])
     vocab_size = inputs.shape[-1]
@@ -188,6 +198,6 @@ def one_hot_multiply(inputs, scale):
     return outputs
 
 def floorMod(a,b):
-    a = torch.tensor(a).float()
-    b= torch.tensor(b).float()
+    #a = torch.tensor(a).float()
+    #b= torch.tensor(b).float()
     return a - (torch.floor(torch.div(a,b))*b)

pytorch/nflib/flows.py
@@ -92,10 +92,12 @@ class AffineHalfFlow(nn.Module):
     - RealNVP both scales and shifts (default)
     - NICE only shifts
     """
-    def __init__(self, dim, parity, net_class=MLP, nh=24, scale=True, shift=True):
+    def __init__(self, dim, parity, net_class=MLP, nh=24, scale=True, shift=True, block_mask=True):
+        # block mask splits in half down the middle. if false then it does even odd. 
         super().__init__()
         self.dim = dim
         self.parity = parity
+        self.block_mask = block_mask
         self.s_cond = lambda x: x.new_zeros(x.size(0), self.dim // 2)
         self.t_cond = lambda x: x.new_zeros(x.size(0), self.dim // 2)
         if scale:
@@ -104,7 +106,11 @@ class AffineHalfFlow(nn.Module):
             self.t_cond = net_class(self.dim // 2, self.dim // 2, nh)
         
     def forward(self, x):
-        x0, x1 = x[:,::2], x[:,1::2]
+        # there is a bug here where the concat operation fails!
+        if self.block_mask: 
+            x0, x1 = x[:,:self.dim//2], x[:,self.dim//2:]
+        else: 
+            x0, x1 = x[:,::2], x[:,1::2]
         if self.parity:
             x0, x1 = x1, x0
         s = self.s_cond(x0)
@@ -113,21 +119,36 @@ class AffineHalfFlow(nn.Module):
         z1 = torch.exp(s) * x1 + t # transform this half as a function of the other
         if self.parity:
             z0, z1 = z1, z0
-        z = torch.cat([z0, z1], dim=1)
+        if self.block_mask:
+            z = torch.cat([z0, z1], dim=1)
+        else: 
+            z = torch.zeros((x.shape[0], self.dim))  
+            z[:,::2] = z0
+            z[:,1::2] = z1
         log_det = torch.sum(s, dim=1)
         return z, log_det
     
     def backward(self, z):
-        z0, z1 = z[:,::2], z[:,1::2]
+        #print(z.shape)
+        if self.block_mask: 
+            z0, z1 = z[:,:self.dim//2], z[:,self.dim//2:]
+        else: 
+            z0, z1 = z[:,::2], z[:,1::2]
         if self.parity:
             z0, z1 = z1, z0
+        #print(z0.shape)
         s = self.s_cond(z0)
         t = self.t_cond(z0)
         x0 = z0 # this was the same
         x1 = (z1 - t) * torch.exp(-s) # reverse the transform on this half
         if self.parity:
             x0, x1 = x1, x0
-        x = torch.cat([x0, x1], dim=1)
+        if self.block_mask:
+            x = torch.cat([x0, x1], dim=1)
+        else: 
+            x = torch.zeros((z.shape[0], self.dim))  
+            x[:,::2] = x0
+            x[:,1::2] = x1
         log_det = torch.sum(-s, dim=1)
         return x, log_det
 